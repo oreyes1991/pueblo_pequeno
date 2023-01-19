5d02ad13b7de4ebe0d33a19e52e57fc953cc2d46
Luis Zurita <luisdzuritax1@gmail.com>
2023-01-18 21:54:06 -0500

    remove 3 Js line

--- codejs.js
+++ codejs.js
@@ -5,9 +5,6 @@ const poput = document.querySelector('.back-Ground');
 const Xboton = document.querySelector('.Xboton');
 
 let currentCabana = undefined;
-console.log (cabanas, cabanas.length);
-console.log (poput, poput.length);
-console.log (Xboton, Xboton.length);
 
 for (let i=0; i < cabanas.length; i++) {
     const aCabana = cabanas[i];
