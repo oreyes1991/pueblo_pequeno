5eb0f6c95c7e4a1085d923f94c0367b425d82020
Luis Zurita <luisdzuritax1@gmail.com>
2023-02-20 22:12:42 -0500

    Area-rent-fix

--- codejs.js
+++ codejs.js
@@ -109,7 +109,7 @@ if(mainGallery){
 /* Rent Area */
 const areaList = [
     {
-      name: 'Psina',
+      name: 'Piscina',
       precio: "25$/5H",
       img:["cabana2.jpg", "cabana3.jpg", "cocina.jpg", "interno.jpg"]
     },
@@ -130,87 +130,48 @@ const areaList = [
     }
   ];
   // recorrer una list
-  areaList.forEach((areaRe) => {
-    console.log(areaRe.name, areaRe.precio , areaRe.img[1]);
-  });
   
   const areasList = areaList.map((areaRe,i) => {
     return `<div class="AreaR">
               <h2>${areaRe.name} </h2>
-              <div class="subContainer precio">
+              <div class="subContainer">
                   <div class="imgReserve2">
-                      <img id="slider${i}" class="imgSlider" />
+                      ${areaRe.img.map((imgSrc, j) => {
+                        return `
+                          <img id="slider-${areaRe.name}-${j}" class="imgSlider hidden" src="img/${imgSrc}" />
+                        `
+                      }).join('')
+                      }
                   </div>
-                  <p class="precio"><b>${areaRe.precio} </b> </p>
+                  <p class="precios"><b>${areaRe.precio} </b> </p>
               </div>
               
           </div>`
   });
   
-  const RentAreas = document.querySelector('.RentAreas');
-  RentAreas.innerHTML = areasList.join(' ');
-
-
-/*psina*/
-const slider0 = document.getElementById("slider0");
-const imgRent0 = ["cabana2.jpg", "cabana3.jpg", "cocina.jpg", "interno.jpg"];
-let rentImg0 = 0;
-
-const changeImg0 = function(){
-    slider0.src = `img/${imgRent0[rentImg0]}`;
-    if (rentImg0 < (imgRent0.length - 1)){
-        rentImg0++;
-    }else {
-        rentImg0 = 0;
-    }
-};
-changeImg0();
-setInterval(changeImg0, 2000);
-
-/*Salon*/
-const slider1 = document.getElementById("slider1");
-const imgRent1 = ["cabana2.jpg", "cabana3.jpg", "cocina.jpg", "interno.jpg"];
-let rentImg1 = 0;
-
-const changeImg1 = function(){
-  slider1.src = `img/${imgRent1[rentImg1]}`;
-  if (rentImg1 < (imgRent0.length - 1)){
-      rentImg1++;
-  }else {
-      rentImg1 = 0;
+const RentAreas = document.querySelector('.RentAreas');
+RentAreas.innerHTML = areasList.join(' ');
+let counter = 0;
+function showElement(areaName, index) {
+  const element = document.querySelector(`#slider-${areaName}-${index}`);
+  element.classList.remove('hidden');
+}
+function hideElement(areaName, index) {
+  const element = document.querySelector(`#slider-${areaName}-${index}`);
+  element.classList.add('hidden');
+}
+areaList.forEach((area) => {
+  showElement(area.name, counter);
+});
+setInterval(() => {
+  areaList.forEach((area) => {
+    hideElement(area.name, counter);
+  });
+  counter += 1;
+  if (counter >= areaList.length) {
+    counter = 0;
   }
-};
-changeImg1();
-setInterval(changeImg1, 2000);
-
-/*parque*/
-const slider2 = document.getElementById("slider2");
-const imgRent2 = ["cabana2.jpg", "cabana3.jpg", "cocina.jpg", "interno.jpg"];
-let rentImg2 = 0;
-
-const changeImg2 = function(){
-  slider2.src = `img/${imgRent2[rentImg2]}`;
-  if (rentImg2 < (imgRent2.length - 1)){
-      rentImg2++;
-  }else {
-      rentImg2 = 0;
-  }
-};
-changeImg2();
-setInterval(changeImg2, 2000);
-
-/*restaurante*/
-const slider3 = document.getElementById("slider3");
-const imgRent3 = ["cabana2.jpg", "cabana3.jpg", "cocina.jpg", "interno.jpg"];
-let rentImg3 = 0;
-
-const changeImg3 = function(){
-  slider3.src = `img/${imgRent3[rentImg3]}`;
-  if (rentImg3 < (imgRent3.length - 1)){
-      rentImg3++;
-  }else {
-      rentImg3 = 0;
-  }
-};
-changeImg3();
-setInterval(changeImg3, 2000);
+  areaList.forEach((area) => {
+    showElement(area.name, counter);
+  });
+}, 8000);
