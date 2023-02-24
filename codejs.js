3c66002486504d701879b56c93af1360a280569a
Wilbert Marcano <wilbert.marcano@gmail.com>
2023-02-24 14:29:11 -0400

    color added

--- codejs.js
+++ codejs.js
@@ -19,7 +19,23 @@ if (Xboton){
     });  
 }
  
-
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap
 
 
 /* Gallery settings */
