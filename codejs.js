cbaa40185bcf630429d672aa5d9c919113c238c0
Luis Zurita <luisdzuritax1@gmail.com>
2023-03-23 19:06:07 -0400

    fixed-review

--- codejs.js
+++ codejs.js
@@ -1,19 +1,19 @@
-const cabanas = document.querySelectorAll(".cabana");
-const poput = document.querySelector(".back-Ground");
-const Xboton = document.querySelector(".Xboton");
+const cabanas = document.querySelectorAll('.cabana');
+const poput = document.querySelector('.back-Ground');
+const xboton = document.querySelector('.Xboton');
 
 let currentCabana = undefined;
 
 for (let i = 0; i < cabanas.length; i++) {
   const aCabana = cabanas[i];
-  aCabana.addEventListener("click", () => {
+  aCabana.addEventListener('click', () => {
     currentCabana = i;
-    poput.classList.remove("hide");
+    poput.classList.remove('hide');
   });
 }
-if (Xboton) {
-  Xboton.addEventListener("click", () => {
-    poput.classList.add("hide");
+if (xboton) {
+  xboton.addEventListener('click', () => {
+    poput.classList.add('hide');
   });
 }
 // Initialize and add the map
@@ -38,44 +38,44 @@ window.initMap = initMap;
 
 const dataGallery = [
   {
-    name: "cabana 1",
-    imgMosaico: "img/cabana2.jpg",
-    imgFullScren: "img/cabana2.jpg",
+    name: 'cabana 1',
+    imgMosaico: 'img/cabana2.jpg',
+    imgFullScren: 'img/cabana2.jpg',
   },
   {
-    name: "cabana 2",
-    imgMosaico: "img/cabana2.jpg",
-    imgFullScren: "img/cabana2.jpg",
+    name: 'cabana 2',
+    imgMosaico: 'img/cabana2.jpg',
+    imgFullScren: 'img/cabana2.jpg',
   },
   {
-    name: "cabana 3",
-    imgMosaico: "img/cabana2.jpg",
-    imgFullScren: "img/cabana2.jpg",
+    name: 'cabana 3',
+    imgMosaico: 'img/cabana2.jpg',
+    imgFullScren: 'img/cabana2.jpg',
   },
   {
-    name: "cabana 4",
-    imgMosaico: "img/interno_04_big.jpg",
-    imgFullScren: "img/interno_04_big.jpg",
+    name: 'cabana 4',
+    imgMosaico: 'img/interno_04_big.jpg',
+    imgFullScren: 'img/interno_04_big.jpg',
   },
   {
-    name: "cabana 5",
-    imgMosaico: "img/cabana3.jpg",
-    imgFullScren: "img/cabana3.jpg",
+    name: 'cabana 5',
+    imgMosaico: 'img/cabana3.jpg',
+    imgFullScren: 'img/cabana3.jpg',
   },
   {
-    name: "cabana 6",
-    imgMosaico: "img/cocina.jpg",
-    imgFullScren: "img/cocina.jpg",
+    name: 'cabana 6',
+    imgMosaico: 'img/cocina.jpg',
+    imgFullScren: 'img/cocina.jpg',
   },
   {
-    name: "cabana 7",
-    imgMosaico: "img/cabana2.jpg",
-    imgFullScren: "img/cabana2.jpg",
+    name: 'cabana 7',
+    imgMosaico: 'img/cabana2.jpg',
+    imgFullScren: 'img/cabana2.jpg',
   },
   {
-    name: "cabana 8",
-    imgMosaico: "img/cabana3.jpg",
-    imgFullScren: "img/cabana3.jpg",
+    name: 'cabana 8',
+    imgMosaico: 'img/cabana3.jpg',
+    imgFullScren: 'img/cabana3.jpg',
   },
 ];
 
@@ -86,33 +86,33 @@ const imgGallery = dataGallery.map((imgPP, i) => {
 </div>
 <div id="a${i}" class="img_Fullscreen back-Ground hide">
     <div class="fulls">
-        <button class="Xboton">X</button>
+        <button class="xboton">X</button>
         <img class="liked-full" src=${imgPP.imgFullScren} />
     </div>
 </div>`;
 });
 
-const mainGallery = document.querySelector(".gallery");
+const mainGallery = document.querySelector('.gallery');
 if (mainGallery) {
   mainGallery.innerHTML = imgGallery.join(" ");
 
   const likedPhoto = document.querySelectorAll(".liked");
-  const eBoton = document.querySelectorAll(".Xboton");
+  const eBoton = document.querySelectorAll('.xboton');
   let currentPhoto = undefined;
 
   for (let i = 0; i < likedPhoto.length; i += 1) {
     const likedF = likedPhoto[i];
-    likedF.addEventListener("click", () => {
+    likedF.addEventListener('click', () => {
       const backGround = document.querySelector(`#a${i}`);
-      backGround.classList.remove("hide");
+      backGround.classList.remove('hide');
     });
   }
   console.log(currentPhoto);
 
   eBoton.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener('click', () => {
       const backGround = document.querySelector(`#a${i}`);
-      backGround.classList.add("hide");
+      backGround.classList.add('hide');
     });
   });
 }
