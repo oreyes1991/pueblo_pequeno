a15940fe90f206c89c4b77f56e40289efdc588a4
Wilbert Marcano <wilbert.marcano@gmail.com>
2023-05-06 15:04:55 -0400

    new photo files added

--- codejs.js
+++ codejs.js
@@ -25,7 +25,7 @@ function initMap() {
     const uluru = { lat: -25.344, lng: 131.031 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 10,
       center: uluru,
     });
     // The marker, positioned at Uluru
